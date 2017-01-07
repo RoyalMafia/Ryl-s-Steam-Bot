f1529f65070e7ca8765a3e2ae6462666b1779c0a
RoyalMafia <benpotten@gmail.com>
2017-01-07 17:28:05 +0000

    danke

--- bot.js
+++ bot.js
@@ -16,10 +16,10 @@ var cleverbot = require( 'cleverbot.io' );
 var client         = new SteamUser();
 var botChatID      = null;
 var botChatAdminID = null;
-var readline = require('readline');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+var readline       = require('readline');
+var rl             = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
 });
 
 /*
@@ -28,7 +28,9 @@ var rl = readline.createInterface({
 
 */
 
-var cmdList = {};
+var cmdList     = {};
+var chatcmdList = {};
+var chatUsrs    = [];
 
 /* 
 
@@ -36,21 +38,8 @@ var cmdList = {};
 
 */
 
-function findCmd( cmd ) {
-	for( i in cmdList ) {
-		if( cmdList[i][0] == cmd ) {
-			return true;
-		};
-	};
-};
-
-function getCmd( cmd ) {
-	for( i in cmdList ) {
-		if( cmdList[i][0] == cmd ) {
-			return i;
-		};
-	};
-};
+function findVal( list, val, pos ) {for( i in list ) {if( list[i][pos] == val ) {return true;};};};
+function getVal( list, val, pos ) {for( i in list ) {if( list[i][pos] == val ) {return i;};};};
 
 /*
 
@@ -63,10 +52,10 @@ cmdList.Cmds = [ "!cmds", "Displays all available commands.", function( steamID 
 	var StrArr = ["Comands:\n"];
 
 	for( i in cmdList ) {
-		StrArr.push( cmdList[i][0]+": "+cmdList[i][1]+"\n" );
+		StrArr.push( cmdList[i][0]+": "+cmdList[i][1] );
 	};
 
-	client.chatMessage( steamID, StrArr.join("") );
+	client.chatMessage( steamID, StrArr.join("\n") );
 }];
 cmdList.chatRoom = [ "!chatroom", "Creates a chat room & invites / Invites (If a chat room already exists) to exist chat room.", function( steamID ) {
 	if( botChatID == null ) {
@@ -97,9 +86,52 @@ cmdList.invChat = [ "!invchat", "Invites you to a chat room if one exists.", fun
 	}
 }];
 
+/*
+
+	Chat room commands
+
+*/
+
+chatcmdList.cmds = ["!cmds", "Displays available commands.", function( steamID ) {
+	var StrArr = ["Chat Room Comands:\n"];
+
+	for( i in chatcmdList ) {
+		StrArr.push( chatcmdList[i][0]+": "+chatcmdList[i][1] );
+	};
+
+	client.chatMessage( steamID, StrArr.join("\n") );
+}];
+
+chatcmdList.kickFromChat = ["!kick", "Kicks the specified user from the chat, usage: !kick {Username}", function( steamID, target ) {
+	if( steamID.toString() != botChatAdminID.toString() ) {
+		console.log( steamID == botChatAdminID );
+		client.chatMessage( steamID, "You don't have access to that command!" );
+		return
+	};
+
+	var targetid = null;
+	var index    = null;
+	
+	for( i in chatUsrs ) {
+		if( chatUsrs[i][0].includes( target )  ) {
+			targetid = chatUsrs[i][1];
+			index = i;
+		};
+	};
+
+	if( targetid != null ) {
+		console.log( "Kicked "+target+" from the chat room." )
+		client.kickFromChat( botChatID, targetid );
+		chatUsrs.splice( index, 1 );
+	}else{
+		console.log( "Target not found." );
+		client.chatMessage( steamID, "Target not found." )
+	};
+}];
+
 /* 
 	
-	Cleverbot.IO 
+	Cleverbot.io
 
 */
 
@@ -130,21 +162,28 @@ function cleverBotResp( steamID, message ) {
 
 */
 
+// Initiate Login
 client.logOn({
     accountName: process.argv[2],
     password: process.argv[3]
 });
 
+// What to do once we've logged in
 client.on('loggedOn', () => {
     client.setPersona(SteamUser.Steam.EPersonaState.Online, "Ryl's Bot");
     console.log("Logged into Steam account.");
+    rl.question("Use cleverbot? (Y/N) ", function( yn ) {
+    	if( yn.toLocaleLowerCase() == "y" ) {
+		    rl.question("Cleverbot Username: ", function(usr) {
+			  	rl.question("Cleverbot API Key: ", function(key) {
+			  		createCleverBot( usr, key );
+					rl.close();
+				});
 
-    rl.question("Cleverbot Username: ", function(usr) {
-	  	rl.question("Cleverbot API Key: ", function(key) {
-	  		createCleverBot( usr, key );
-			rl.close();
-		});
-
+			});
+		}else{
+			rl.close()
+		};
 	});
 });
 
@@ -157,24 +196,80 @@ client.on('friendMessage', function(steamID, message) {
 		console.log("Message from " + name + ": " + message);
 	});
 
-    if( findCmd( message ) ) {
-    	cmdList[getCmd( message )][2]( steamID );
-    }else{
-    	cleverBotResp( steamID, message );
-    };
+	if( message[0] == "!" ) {
+	    if( findVal( cmdList, message, 0 ) ) {
+	    	cmdList[getVal( cmdList, message, 0 )][2]( steamID );
+	    }else{
+	    	client.chatMessage( steamID, "Invalid command entered, use !cmds to get chat commands." );
+	    };
+	}else{
+		cleverBotResp( steamID, message );
+	};
 
 });
 
 // Room Functions
 client.on('chatMessage', function( room, chatter, message ) {
-	cleverBotResp( room, message );
+	client.getPersonas([chatter], function(personas) {
+		var persona = personas[chatter];
+		var name = persona ? persona.player_name : ("[" + chatter + "]");
+
+		console.log("Chat room message from " + name + ": " + message);
+	});
+
+	var strExploded = message.split( " " );
+
+	if( strExploded[0][0] == "!" ) {
+		if( findVal( chatcmdList, strExploded[0], 0 ) ) {
+			chatcmdList[getVal( chatcmdList, strExploded[0], 0 )][2]( chatter, strExploded[1] );
+		}else{
+			client.chatMessage( chatter, "Invalid command entered, use !cmds to get chat commands."  );
+		};
+	}else{
+		cleverBotResp( room, message );
+	};
 });
 
+// User join chat room
 client.on('chatUserJoined', function( chatID, userID ) {
 	client.getPersonas([userID], function(personas) {
 		var persona = personas[userID];
 		var name = persona ? persona.player_name : ("[" + userID + "]");
 
 		client.chatMessage( chatID, "Welcome "+name+"!" );
+
+		chatUsrs.push( [name, userID] );
 	});
+});
+
+// User leave chat room
+client.on('chatUserLeft', function( chatID, userID ) {
+	
+});
+
+// Auto Accept Friend Request
+client.on('friendRelationship', function( steamID, relationship ) {
+    if ( relationship == SteamUser.Steam.EFriendRelationship.RequestRecipient ) {
+        client.addFriend(steamID);
+		console.log("Accepted friend request from " + steamID.getSteam3RenderedID());
+    };
+});
+
+// Chat Left
+client.on('chatLeft', function( chatID ) {
+	if( chatID == botChatID ) {
+		botChatID      = null;
+		botChatAdminID = null;
+
+		console.log( "Chat no longer exists, resetting variables." );
+	};
+});
+
+// Disconnect
+client.on('disconnected', function( eresult, msg ) {
+	console.log( "Disconnected from Steam resetting variables." )
+	
+	botChatID = null;
+	botChatAdminID = null;
+	chatUsrs = null;
 });
